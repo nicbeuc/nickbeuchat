e4856f91f77ffbd3ac1765b0099b19d09a690637
nicbeuc <nick.beuchat@gmail.com>
2023-08-05 20:47:43 -0400

    more updates

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,11 +1,11 @@
 import styles from './Intro.module.css';
-import { ASSOCIATIONS } from '@/constants';
+import { ASSOCIATIONS, INTRO_TEXT } from '@/constants';
 
 export default function Intro() {
   return (
     <div className={styles.introWrapper}>
       <div className={styles.text}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>{ INTRO_TEXT }</p>
       </div>
       <div className={styles.assoc}>
         <h2 className={styles.assocTitle}>Associations</h2>
